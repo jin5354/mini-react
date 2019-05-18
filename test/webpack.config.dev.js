691ca9ff43dd41d0f4a32f0d7b8cdbb24e87de89
jin5354 <xiaoyanjinx@gmail.com>
2019-05-18 15:41:15 +0800

    feat(init): init project

---
+++ test/webpack.config.dev.js
@@ -0,0 +1,31 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, './test.js'),
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, './'),
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader'
+    }]
+  },
+  devServer: {
+    contentBase: __dirname,
+    historyApiFallback: true,
+    inline: true,
+    publicPath: "/",
+    host: '0.0.0.0',
+    port: 9093
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './index.html'),
+    })
+  ]
+}
